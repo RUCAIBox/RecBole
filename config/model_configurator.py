fdf8673fd4fa2790c877b4f2a839e075c288c61a
Lin zihan <893833413@qq.com>
2020-07-10 18:43:56 +0800

    FIX: config rebuild

--- config/model_configurator.py
+++ config/model_configurator.py
@@ -1,30 +1,22 @@
-from configparser import ConfigParser
 from config.abstract_configurator import AbstractConfig
 
 
 class ModelConfig(AbstractConfig):
-    def __init__(self, config_file_name):
+    def __init__(self, config_file_name, cmd_args):
         super().__init__()
-        self.must_args = []
+        self.must_args = ['train_batch_size']
         self.args = self._read_config_file(config_file_name, 'model')
-        for cmd_arg_name, cmd_arg_value in self.cmd_args.items():
-            if cmd_arg_name.startswith('model.'):
-                self.args[cmd_arg_name] = cmd_arg_value
-
-        self.default_args = self.default_parser.getargs()
-        for default_args in self.default_args.keys():
-
-            if str(default_args) not in self.args and str(default_args).startswith('model.'):
-                self.args[str(default_args)] = str(self.default_args[default_args])
 
         self._check_args()
+        self._replace_args(cmd_args)
 
     def dump_config_file(self, config_file):
         """
         This function can dump the model's hyper parameters to a new config file
         :param config_file: file name that write to.
         """
-        model_config = ConfigParser()
-        model_config['model'] = self.args
-        with open(config_file, 'w') as configfile:
-            model_config.write(configfile)
+        # model_config = ConfigParser()
+        # model_config['model'] = self.args
+        # with open(config_file, 'w') as configfile:
+        #     model_config.write(configfile)
+        raise NotImplementedError
