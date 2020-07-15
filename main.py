8c89ab1c491a02fadcb4b0d38a4b9673654f424c
hyp1231 <houyupeng@ruc.edu.cn>
2020-07-15 11:37:04 +0800

    FEA: Add Point-wise DataLoader & FIX: Refactored Data.build Interface

--- main.py
+++ main.py
@@ -11,7 +11,17 @@ logger = Logger(config)
 
 dataset = Dataset(config)
 
-train_data, test_data, valid_data = dataset.build()
+train_data, test_data, valid_data = dataset.build(
+    inter_filter_lowest_val=config['lowest_val'],
+    inter_filter_highest_val=config['highest_val'],
+    split_by_ratio=[config['train_split_ratio'], config['valid_split_ratio'], config['test_split_ratio']],
+    train_batch_size=config['train_batch_size'],
+    test_batch_size=config['test_batch_size'],
+    valid_batch_size=config['valid_batch_size'],
+    pairwise=True,
+    neg_sample_by=1,
+    neg_sample_to=config['test_neg_sample_num']
+)
 
 model = BPRMF(config, dataset).to(config['device'])
 
