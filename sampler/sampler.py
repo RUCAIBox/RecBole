84ae16d3962d1321c603822c9f8072aef75a8431
chenyushuo <297086016@qq.com>
2020-07-29 13:58:53 +0800

    Speed up on GeneralGroupedDataLoader()._neg_sampling()

--- sampler/sampler.py
+++ sampler/sampler.py
@@ -30,6 +30,7 @@ class Sampler(object):
         random.shuffle(self.random_item_list)
         self.random_pr = 0
 
+        self.full_set = set(range(self.n_items))
         self.used_item_id = dict()
         last = [set() for i in range(self.n_users)]
         for phase, dataset in zip(self.phases, self.datasets):
@@ -67,6 +68,16 @@ class Sampler(object):
                 cur = self.random_item()
                 if cur not in self.used_item_id[phase][user_id]:
                     return cur
+        except KeyError:
+            if phase not in self.phases:
+                raise ValueError('phase [{}] not exist'.format(phase))
+        except IndexError:
+            if user_id < 0 or user_id >= self.n_users:
+                raise ValueError('user_id [{}] not exist'.format(user_id))
+
+    def sample_full_by_user_id(self, phase, user_id):
+        try:
+            return list(self.full_set - self.used_item_id[phase][user_id])
         except KeyError:
             if phase not in self.phases:
                 raise ValueError('phase [{}] not exist'.format(phase))
