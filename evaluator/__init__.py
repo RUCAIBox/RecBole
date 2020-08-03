b4c630f6760ed0b07b7ebd7212338c60e53a8d90
tsotfsk <1337990880@qq.com>
2020-08-03 22:59:54 +0800

    FEA: speed up evaluator

--- evaluator/__init__.py
+++ evaluator/__init__.py
@@ -1,1 +1,4 @@
-from .evaluator import TopKEvaluator, LossEvaluator
+from .loss_evaluator import LossEvaluator
+from .topk_evaluator import TopKEvaluator
+from .utils import *
+from .metrics import *
