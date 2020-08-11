fcccbd8b8d18389c95f178248a663add81e27ff7
Lin zihan <893833413@qq.com>
2020-08-11 09:55:19 +0800

    simplify config

--- config/running_configurator.py
+++ config/running_configurator.py
@@ -13,9 +13,17 @@ class RunningConfig(AbstractConfig):
 
         self._check_args()
         self._replace_args(cmd_args)
+        if 'gpu_id' in self:
+            self['use_gpu'] = True
+        else:
+            self['use_gpu'] = False
+
         if 'data_path' not in self:
             data_path = os.path.join('dataset', self['dataset'])
             self['data_path'] = data_path
+
+        if 'checkpoint_dir' not in self:
+            self['checkpoint_dir'] = 'saved'
 
         eval_type = None
         for metric in self['metrics']:
@@ -30,4 +38,22 @@ class RunningConfig(AbstractConfig):
                 else:
                     eval_type = EvaluatorType.RANKING
         self['eval_type'] = eval_type
+
+        smaller_metric = ['rmse','mae', 'logloss']
+
+        if 'valid_metric' not in self:
+            valid_metric = self['metric'][0]
+            if 'topk' in self:
+                valid_metric += '@' + self['topk'][0]
+            self['valid_metric'] = valid_metric
+
+        if 'valid_metric_bigger' not in self:
+            valid_metric = self['valid_metric'].split('@')[0]
+            if valid_metric in smaller_metric:
+                self['valid_metric_bigger'] = False
+            else:
+                self['valid_metric_bigger'] = True
+
+
+
 
