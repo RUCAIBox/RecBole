7133525968fa66c54ca9bad88fee54e41abcc5b9
Lin zihan <893833413@qq.com>
2020-08-13 09:43:12 +0800

    FEA: add cmd_configurator

--- config/running_configurator.py
+++ config/running_configurator.py
@@ -5,14 +5,14 @@ from utils import EvaluatorType
 
 
 class RunningConfig(AbstractConfig):
-    def __init__(self, config_file_name, cmd_args):
+    def __init__(self, config_file_name):
 
         super().__init__()
         self.must_args = ['model', 'dataset']
         self.args = self._read_config_file(config_file_name, 'default')
 
         self._check_args()
-        self._replace_args(cmd_args)
+        #self._replace_args(cmd_args)
         if 'gpu_id' in self:
             self['use_gpu'] = True
         else:
