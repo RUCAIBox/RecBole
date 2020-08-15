191d3490140bba69726a3c28fd7320513b427cef
hyp1231 <houyupeng@ruc.edu.cn>
2020-08-15 08:25:41 +0430

    FIX: bugs in data config

--- config/data_configurator.py
+++ config/data_configurator.py
@@ -2,10 +2,10 @@ from config.abstract_configurator import AbstractConfig
 
 
 class DataConfig(AbstractConfig):
-    def __init__(self, config_file_name):
+    def __init__(self, config_file_name, cmd_args):
         super().__init__()
         self.must_args = ['USER_ID_FIELD', 'ITEM_ID_FIELD']
         self.args = self._read_config_file(config_file_name, 'data')
         self._check_args()
 
-        #self._replace_args(cmd_args)
+        self._replace_args(cmd_args)
