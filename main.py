15bb165b950f347bd6ea5304da2208d4e2d6e56c
2017pxy <2017202006@ruc.edu.cn>
2020-09-01 23:53:26 +0800

    update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from logging import getLogger
 from recbox.config import Config
-from recbox.data import Dataset, data_preparation
+from recbox.data import Dataset, data_preparation, SocialDataset
 from recbox.model.general_recommender.bprmf import BPRMF
 from recbox.trainer import Trainer
 from recbox.utils import init_logger, get_model
@@ -12,7 +12,7 @@ logger = getLogger()
 
 dataset = Dataset(config)
 logger.info(dataset)
-'''
+
 # If you want to customize the evaluation setting,
 # please refer to `data_preparation()` in `data/utils.py`.
 train_data, test_data, valid_data = data_preparation(config, dataset)
@@ -26,4 +26,3 @@ trainer = Trainer(config, model)
 best_valid_score, _ = trainer.fit(train_data, valid_data)
 result = trainer.evaluate(test_data)
 logger.info(best_valid_score)
-'''
