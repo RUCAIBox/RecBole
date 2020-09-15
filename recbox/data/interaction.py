b648cae7af54d3130d0985f69bb63b4281469d49
hyp1231 <houyupeng@ruc.edu.cn>
2020-09-15 14:31:31 +0430

    FIX: add attr support for Interaction's api

--- recbox/data/interaction.py
+++ recbox/data/interaction.py
@@ -7,6 +7,7 @@
 # @Author  : Yupeng Hou, Yushuo Chen, Xingyu Pan
 # @email   : houyupeng@ruc.edu.cn, chenyushuo@ruc.edu.cn, panxy@ruc.edu.cn
 
+import numpy as np
 
 class Interaction(object):
     """The basic class representing a batch of interaction records.
@@ -65,6 +66,8 @@ class Interaction(object):
         self.interaction = interaction
         self.pos_len_list = pos_len_list
         self.user_len_list = user_len_list
+        if (self.pos_len_list is None) ^ (self.user_len_list is None):
+            raise ValueError('pos_len_list and user_len_list should be both None or valued.')
         for k in self.interaction:
             self.length = self.interaction[k].shape[0]
             break
@@ -171,7 +174,9 @@ class Interaction(object):
                 ret[k] = self.interaction[k].repeat(sizes)
             else:
                 ret[k] = self.interaction[k].repeat([sizes, 1])
-        return Interaction(ret)
+        new_pos_len_list = self.pos_len_list * sizes if self.pos_len_list else None
+        new_user_len_list = self.user_len_list * sizes if self.user_len_list else None
+        return Interaction(ret, new_pos_len_list, new_user_len_list)
 
     def repeat_interleave(self, repeats, dim=0):
         """Similar to repeat_interleave of PyTorch.
@@ -185,10 +190,14 @@ class Interaction(object):
         ret = {}
         for k in self.interaction:
             ret[k] = self.interaction[k].repeat_interleave(repeats, dim=dim)
-        return Interaction(ret)
+        new_pos_len_list = list(np.multiply(self.pos_len_list, repeats)) if self.pos_len_list else None
+        new_user_len_list = list(np.multiply(self.user_len_list, repeats)) if self.user_len_list else None
+        return Interaction(ret, new_pos_len_list, new_user_len_list)
 
     def update(self, new_inter):
         """Similar to ``dict.update()``
         """
         for k in new_inter.interaction:
             self.interaction[k] = new_inter.interaction[k]
+        self.pos_len_list = new_inter.pos_len_list
+        self.user_len_list = new_inter.user_len_list
