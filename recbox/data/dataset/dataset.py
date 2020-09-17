0bc1badc55dfb70b20b4c5d33790201e39e8ab9f
hyp1231 <houyupeng@ruc.edu.cn>
2020-09-17 13:21:45 +0430

    FEA: name of dataset in dataset's __str__

--- recbox/data/dataset/dataset.py
+++ recbox/data/dataset/dataset.py
@@ -3,7 +3,7 @@
 # @Email  : houyupeng@ruc.edu.cn
 
 # UPDATE:
-# @Time   : 2020/9/15, 2020/9/15, 2020/9/16
+# @Time   : 2020/9/17, 2020/9/15, 2020/9/16
 # @Author : Yupeng Hou, Xingyu Pan, Yushuo Chen
 # @Email  : houyupeng@ruc.edu.cn, panxy@ruc.edu.cn, chenyushuo@ruc.edu.cn
 
@@ -671,7 +671,7 @@ class Dataset(object):
         return self.__str__()
 
     def __str__(self):
-        info = []
+        info = [self.dataset_name]
         if self.uid_field:
             info.extend(['The number of users: {}'.format(self.user_num),
                          'Average actions of users: {}'.format(self.avg_actions_of_users)])
