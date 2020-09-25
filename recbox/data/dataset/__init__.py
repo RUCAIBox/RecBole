7b3accac6d621446bf8e82eb5bd2ee92c228c72d
2017pxy <2017202006@ruc.edu.cn>
2020-09-25 15:43:08 +0800

    FEA: add kg_seq_dataset

--- recbox/data/dataset/__init__.py
+++ recbox/data/dataset/__init__.py
@@ -2,3 +2,4 @@ from recbox.data.dataset.dataset import Dataset
 from recbox.data.dataset.sequential_dataset import SequentialDataset
 from recbox.data.dataset.kg_dataset import KnowledgeBasedDataset
 from recbox.data.dataset.social_dataset import SocialDataset
+from recbox.data.dataset.kg_seq_dataset import Kg_Seq_Dataset
