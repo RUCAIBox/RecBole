0a0e2abbe3d81268ceaef598b07446b8833d6a5a
Lin zihan <893833413@qq.com>
2020-09-26 22:30:45 +0800

    FIX： new version of PNN

--- recbox/model/context_aware_recommender/pnn.py
+++ recbox/model/context_aware_recommender/pnn.py
@@ -10,6 +10,9 @@ Qu Y et al. "Product-based neural networks for user response prediction." in ICD
 
 Note:
 
+Reference code:
+https://github.com/shenweichen/DeepCTR-Torch/blob/master/deepctr_torch/models/pnn.py
+https://github.com/Atomu2014/product-nets/blob/master/python/models.py
 """
 
 import torch
@@ -30,15 +33,20 @@ class PNN(ContextRecommender):
         self.dropout = config['dropout']
         self.use_inner = config['use_inner']
         self.use_outer = config['use_outer']
-        self.d = self.mlp_hidden_size[0]
-        self.product_linear = nn.Linear(self.embedding_size * self.num_feature_field, self.d, bias=False)
-        self.product_bias = torch.nn.Parameter(torch.randn(self.d), requires_grad=True)
-        if self.use_inner:
-            self.product_inner = nn.Linear(self.embedding_size * self.num_feature_field, self.d, bias=False)
-        if self.use_outer:
-            self.product_outer = nn.Linear(self.embedding_size * self.embedding_size, self.d, bias=False)
-        size_list = [self.d] + self.mlp_hidden_size
+        self.reg = config['weight_decay']
 
+        self.num_pair = int(self.num_feature_field * (self.num_feature_field - 1) / 2)
+
+        product_out_dim = self.num_feature_field * self.embedding_size
+        if self.use_inner:
+            product_out_dim += self.num_pair
+            self.inner_product = InnerProductLayer(self.num_feature_field, device=self.device)
+
+        if self.use_outer:
+            product_out_dim += self.num_pair
+            self.outer_product = OuterProductLayer(
+                self.num_feature_field, self.embedding_size, device=self.device)
+        size_list = [product_out_dim] + self.mlp_hidden_size
         self.mlp_layers = MLPLayers(size_list, self.dropout, bn=False)
         self.predict_layer = nn.Linear(self.mlp_hidden_size[-1], 1)
         self.relu = nn.ReLU()
@@ -46,6 +54,19 @@ class PNN(ContextRecommender):
         self.loss = nn.BCELoss()
 
         self.apply(self.init_weights)
+
+    def reg_loss(self):
+        """Calculate the L2 normalization loss of model parameters.
+        Including weight matrixes of mlp layers.
+
+        Returns:
+            loss(torch.FloatTensor): The L2 Loss tensor. shape of [1,]
+        """
+        reg_loss = 0
+        for name, parm in self.mlp_layers.named_parameters():
+            if name.endswith('weight'):
+                reg_loss = reg_loss + self.reg * parm.norm(2)
+        return reg_loss
 
     def init_weights(self, module):
         if isinstance(module, nn.Embedding):
@@ -64,22 +85,20 @@ class PNN(ContextRecommender):
             all_embeddings.append(sparse_embedding)
         if dense_embedding is not None and len(dense_embedding.shape) == 3:
             all_embeddings.append(dense_embedding)
-        pnn_all_embeddings = torch.cat(all_embeddings, dim=1)  # [batch_size, num_field, embed_dim]
+        pnn_all_embeddings = torch.cat(all_embeddings, dim=1)   # [batch_size, num_field, embed_dim]
         batch_size = pnn_all_embeddings.shape[0]
         # linear part
-        lz = self.first_order_linear(pnn_all_embeddings.view(batch_size, -1))  # [batch_size,d]
-        output = lz
+        linear_part = pnn_all_embeddings.view(batch_size, -1)  # [batch_size,num_field*embed_dim]
+        output = [linear_part]
         # second order part
         if self.use_inner:
-            inner_product = self.product_inner(pnn_all_embeddings.view(batch_size, -1))  # [batch_size,d]
-            output = output + inner_product
+            inner_product = self.inner_product(pnn_all_embeddings).view(batch_size, -1)  # [batch_size,num_pairs]
+            output.append(inner_product)
         if self.use_outer:
-            sum_embedding = torch.sum(pnn_all_embeddings, dim=1)  # [batch_size, embed_dim]
-            sum_embedding_matrix = torch.bmm(sum_embedding.unsqueeze(2), sum_embedding.unsqueeze(1))  # [batch_size,embed_dim,embed_dim]
-            outer_product = self.product_outer(sum_embedding_matrix.view(batch_size, -1))  # [batch_size,d]
-            output = output + outer_product
-        output = output + self.product_bias  # [batch_size,d]
-        output = self.relu(output)
+            outer_product = self.outer_product(pnn_all_embeddings).view(batch_size, -1)  # [batch_size,num_pairs]
+            output.append(outer_product)
+        output = torch.cat(output, dim=1)   # [batch_size,d]
+
         output = self.predict_layer(self.mlp_layers(output))  # [batch_size,1]
         output = self.sigmoid(output)
         return output.squeeze()
@@ -88,7 +107,97 @@ class PNN(ContextRecommender):
         label = interaction[self.LABEL]
         output = self.forward(interaction)
 
-        return self.loss(output, label)
+        return self.loss(output, label) + self.reg_loss()
 
     def predict(self, interaction):
         return self.forward(interaction)
+
+
+class InnerProductLayer(nn.Module):
+    """InnerProduct Layer used in PNN that compute the element-wise
+    product or inner product between feature vectors.
+
+    """
+    def __init__(self, num_feature_field, device):
+        """
+        Args:
+            num_feature_field(int) :number of feature fields.
+            device(torch.device) : device object of the model.
+        """
+        super(InnerProductLayer, self).__init__()
+        self.num_feature_field = num_feature_field
+        self.to(device)
+
+    def forward(self, feat_emb):
+        """
+        Args:
+            feat_emb(torch.FloatTensor) :3D tensor with shape: [batch_size,num_pairs,embedding_size].
+
+        Returns:
+            inner_product(torch.FloatTensor): The inner product of input tensor. shape of [batch_size, num_pairs]
+        """
+        # num_pairs = num_feature_field * (num_feature_field-1) / 2
+        row = []
+        col = []
+        for i in range(self.num_feature_field - 1):
+            for j in range(i + 1, self.num_feature_field):
+                row.append(i)
+                col.append(j)
+        p = feat_emb[:, row]  # [batch_size, num_pairs, emb_dim]
+        q = feat_emb[:, col]  # [batch_size, num_pairs, emb_dim]
+
+        inner_product = p * q
+
+        return inner_product.sum(dim=-1)  # [batch_size, num_pairs]
+
+
+class OuterProductLayer(nn.Module):
+    """OutterProduct Layer used in PNN.This implemention is
+    adapted from code that the author of the paper published on https://github.com/Atomu2014/product-nets.
+    """
+
+    def __init__(self, num_feature_field, embedding_size, device):
+        """
+        Args:
+            num_feature_field(int) :number of feature fields.
+            embedding_size(int) :number of embedding size.
+            device(torch.device) : device object of the model.
+        """
+        super(OuterProductLayer, self).__init__()
+
+        self.num_feature_field = num_feature_field
+        num_pairs = int(num_feature_field * (num_feature_field - 1) / 2)
+        embed_size = embedding_size
+
+        self.kernel = nn.Parameter(torch.rand(embed_size, num_pairs, embed_size), requires_grad=True)
+        nn.init.xavier_uniform_(self.kernel)
+
+        self.to(device)
+
+    def forward(self, feat_emb):
+        """
+        Args:
+            feat_emb(torch.FloatTensor) :3D tensor with shape: [batch_size,num_pairs,embedding_size].
+
+        Returns:
+            outer_product(torch.FloatTensor): The outer product of input tensor. shape of [batch_size, num_pairs]
+        """
+        row = []
+        col = []
+        for i in range(self.num_feature_field - 1):
+            for j in range(i + 1, self.num_feature_field):
+                row.append(i)
+                col.append(j)
+        p = feat_emb[:, row]  # [batch_size, num_pairs, emb_dim]
+        q = feat_emb[:, col]  # [batch_size, num_pairs, emb_dim]
+
+        # -------------------------
+
+        p.unsqueeze_(dim=1)  # [batch_size, 1, num_pairs, emb_dim]
+
+        p = torch.mul(p, self.kernel.unsqueeze(0))  # [batch_size,emb_dim,num_pairs,emb_dim]
+        p = torch.sum(p, dim=-1)  # [batch_size,emb_dim,num_pairs]
+        p = torch.transpose(p, 2, 1)  # [batch_size,num_pairs,emb_dim]
+
+        outer_product = p * q
+        return outer_product.sum(dim=-1)  # [batch_size,num_pairs]
