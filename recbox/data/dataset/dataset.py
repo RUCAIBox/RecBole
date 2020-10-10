0acec3547698563ac944114e710d20b303b09cd4
hyp1231 <houyupeng@ruc.edu.cn>
2020-10-10 08:21:31 +0430

    FIX: bugs in updating feat_list

--- recbox/data/dataset/dataset.py
+++ recbox/data/dataset/dataset.py
@@ -3,7 +3,7 @@
 # @Email  : houyupeng@ruc.edu.cn
 
 # UPDATE:
-# @Time   : 2020/10/9, 2020/9/15, 2020/9/23
+# @Time   : 2020/10/10, 2020/9/15, 2020/9/23
 # @Author : Yupeng Hou, Xingyu Pan, Yushuo Chen
 # @Email  : houyupeng@ruc.edu.cn, panxy@ruc.edu.cn, chenyushuo@ruc.edu.cn
 
@@ -253,6 +253,9 @@ class Dataset(object):
             flag = True
             self.logger.debug('ordering item features by user id.')
         if flag:
+            # CANNOT be removed
+            # user/item feat has been updated, thus feat_list should be updated too.
+            self.feat_list = self._build_feat_list()
             self._fill_nan_flag = True
 
     def _preload_weight_matrix(self):
