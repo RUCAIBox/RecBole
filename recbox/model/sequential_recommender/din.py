1714ed39db89b102bc46fc23155f92de0d8996bd
aHuiWang <hui.wang@ruc.edu.cn>
2020-10-15 18:09:08 +0800

    FIX: sequential recommenders code format

--- recbox/model/sequential_recommender/din.py
+++ recbox/model/sequential_recommender/din.py
@@ -20,16 +20,12 @@ https://github.com/shenweichen/DeepCTR-Torch/tree/master/deepctr_torch/models
 
 """
 
-import pandas as pd
-import numpy as np
-
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from torch.nn.init import xavier_normal_, constant_
 
-from recbox.utils import ModelType, InputType, FeatureType
-from recbox.model.layers import FMEmbedding, MLPLayers, SequenceAttLayer, ContextSeqEmbLayer
+from recbox.utils import InputType
+from recbox.model.layers import MLPLayers, SequenceAttLayer, ContextSeqEmbLayer
 from recbox.model.abstract_recommender import SequentialRecommender
 
 
@@ -47,21 +43,13 @@ class DIN(SequentialRecommender):
     input_type = InputType.POINTWISE
 
     def __init__(self, config, dataset):
-        super(DIN, self).__init__()
+        super(DIN, self).__init__(config, dataset)
 
         # get field names and parameter value from config
-        self.USER_ID = config['USER_ID_FIELD']
-        self.ITEM_ID = config['ITEM_ID_FIELD']
         self.LABEL_FIELD = config['LABEL_FIELD']
-        self.ITEM_ID_LIST = self.ITEM_ID + config['LIST_SUFFIX']
-        self.ITEM_LIST_LEN = config['ITEM_LIST_LENGTH_FIELD']
-        self.TARGET_ITEM_ID = self.ITEM_ID
-        self.max_item_list_length = config['MAX_ITEM_LIST_LENGTH']
         self.embedding_size = config['embedding_size']
         self.mlp_hidden_size = config['mlp_hidden_size']
         self.device = config['device']
-        self.embedding_size = config['embedding_size']
-        self.max_len = config['MAX_ITEM_LIST_LENGTH']
         self.dropout = config['dropout']
         # self.dataset = dataset
 
@@ -80,7 +68,7 @@ class DIN(SequentialRecommender):
             4 * num_item_feature * self.embedding_size
         ] + self.mlp_hidden_size
 
-        mask_mat = torch.arange(self.max_len).to(self.device).view(
+        mask_mat = torch.arange(self.max_seq_length).to(self.device).view(
             1, -1)  # init mask
         self.attention = SequenceAttLayer(mask_mat,
                                           self.att_list,
@@ -92,7 +80,7 @@ class DIN(SequentialRecommender):
                                         dropout=self.dropout,
                                         bn=True)
 
-        self.embedding_layer = ContextSeqEmbLayer(dataset, config)
+        self.embedding_layer = ContextSeqEmbLayer(config, dataset)
         self.dnn_predict_layers = nn.Linear(self.mlp_hidden_size[-1], 1)
         self.sigmoid = nn.Sigmoid()
         self.loss = nn.BCELoss()
@@ -107,17 +95,14 @@ class DIN(SequentialRecommender):
             if module.bias is not None:
                 constant_(module.bias.data, 0)
 
-    def forward(self, interaction):
-        item_list = interaction[self.ITEM_ID_LIST]
-        user_list = interaction[self.USER_ID]
-        item_list_len = interaction[self.ITEM_LIST_LEN]
-        target_item_list = interaction[self.TARGET_ITEM_ID]
-        max_length = item_list.shape[1]
+    def forward(self, user, item_seq, item_seq_len, next_items):
 
-        # concatenate the history item list with the target item to get embedding together
-        item_target_list = torch.cat((item_list, target_item_list.unsqueeze(1)), dim=-1)
+        max_length = item_seq.shape[1]
 
-        sparse_embedding, dense_embedding = self.embedding_layer(user_list, item_target_list)
+        # concatenate the history item seq with the target item to get embedding together
+        item_seq_next_item = torch.cat((item_seq, next_items.unsqueeze(1)), dim=-1)
+
+        sparse_embedding, dense_embedding = self.embedding_layer(user, item_seq_next_item)
 
         # concat the sparse embedding and float embedding
         feature_table = {}
@@ -139,7 +124,7 @@ class DIN(SequentialRecommender):
         target_item_feat_emb = target_item_feat_emb.squeeze()
 
         # attention
-        user_emb = self.attention(target_item_feat_emb, item_feat_list, item_list_len)
+        user_emb = self.attention(target_item_feat_emb, item_feat_list, item_seq_len)
         user_emb = user_emb.squeeze()
 
         # input the DNN to get the prediction score
@@ -153,11 +138,20 @@ class DIN(SequentialRecommender):
 
     def calculate_loss(self, interaction):
         label = interaction[self.LABEL_FIELD]
-        output = self.forward(interaction)
+        item_seq = interaction[self.ITEM_SEQ]
+        user = interaction[self.USER_ID]
+        item_seq_len = interaction[self.ITEM_SEQ_LEN]
+        next_items = interaction[self.POS_ITEM_ID]
+
+        output = self.forward(user, item_seq, item_seq_len, next_items)
         loss = self.loss(output, label)
         return loss
 
     # TODO: Merge the two predict functions when the data interface is ready
     def predict(self, interaction):
-        scores = self.forward(interaction)
+        item_seq = interaction[self.ITEM_SEQ]
+        user = interaction[self.USER_ID]
+        item_seq_len = interaction[self.ITEM_SEQ_LEN]
+        next_items = interaction[self.POS_ITEM_ID]
+        scores = self.forward(user, item_seq, item_seq_len, next_items)
         return scores
