c104c33c6584aa59ff76bf1954886c612e80e821
shanlei <2015201909@ruc.edu.cn>
2020-10-19 16:18:46 +0800

    FEA: add test for config module

---
+++ tests/config/test_command_line.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+python test_command_line.py --use_gpu=False --valid_metric=Recall@10 --split_ratio=[0.7,0.2,0.1] \
+--metrics=['Recall@10'] \
+--epochs=200 --eval_setting='LO_RS' --learning_rate=0.3
