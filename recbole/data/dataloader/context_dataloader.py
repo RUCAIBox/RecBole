fa6f7f19357fd1a680298c9080174ec15cc435fb
EliverQ <2537419753@qq.com>
2020-12-10 15:42:23 +0800

    FEA:add sparse adam

--- recbole/data/dataloader/context_dataloader.py
+++ recbole/data/dataloader/context_dataloader.py
@@ -12,7 +12,7 @@ recbole.data.dataloader.context_dataloader
 ################################################
 """
 
-from recbole.data.dataloader.general_dataloader import GeneralDataLoader, GeneralNegSampleDataLoader
+from recbole.data.dataloader.general_dataloader import GeneralDataLoader, GeneralNegSampleDataLoader, GeneralFullDataLoader
 
 
 class ContextDataLoader(GeneralDataLoader):
@@ -25,6 +25,14 @@ class ContextDataLoader(GeneralDataLoader):
 class ContextNegSampleDataLoader(GeneralNegSampleDataLoader):
     """:class:`ContextNegSampleDataLoader` is inherit from
     :class:`~recbole.data.dataloader.general_dataloader.GeneralNegSampleDataLoader`,
+    and didn't add/change anything at all.
+    """
+    pass
+
+
+class ContextFullDataLoader(GeneralFullDataLoader):
+    """:class:`ContextFullDataLoader` is inherit from
+    :class:`~recbole.data.dataloader.general_dataloader.GeneralFullDataLoader`,
     and didn't add/change anything at all.
     """
     pass
