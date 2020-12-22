09e958ce7b9d6b27f83d9addef6eb6466ae6e5a5
Deklan Webster <deklanw@gmail.com>
2020-12-22 18:50:12 -0500

    add EASE

--- recbole/model/general_recommender/__init__.py
+++ recbole/model/general_recommender/__init__.py
@@ -12,3 +12,4 @@ from recbole.model.general_recommender.neumf import NeuMF
 from recbole.model.general_recommender.ngcf import NGCF
 from recbole.model.general_recommender.pop import Pop
 from recbole.model.general_recommender.spectralcf import SpectralCF
+from recbole.model.general_recommender.ease import EASE
