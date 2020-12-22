bd40a3ad47b4f32227e72474b2731515813018c8
guijiql <970955517@qq.com>
2020-12-22 11:21:01 +0800

    FEA: add ranking metric test

---
+++ tests/metrics/test_rank_metrics.py
@@ -0,0 +1,44 @@
+# -*- encoding: utf-8 -*-
+# @Time    :   2020/12/21
+# @Author  :   Zhichao Feng
+# @email   :   fzcbupt@gmail.com
+
+
+import os
+import sys
+import unittest
+
+sys.path.append(os.getcwd())
+import numpy as np
+from recbole.evaluator.metrics import metrics_dict
+
+
+class TestCases(object):
+    user_len_list0 = np.array([2, 3, 5])
+    pos_len_list0 = np.array([1, 2, 3])
+    pos_rank_sum0 = np.array([1, 4, 9])
+
+    user_len_list1 = np.array([3, 6, 4])
+    pos_len_list1 = np.array([1, 0, 4])
+    pos_rank_sum1 = np.array([3, 0, 6])
+
+
+def get_result(name, case=0):
+    func = metrics_dict[name]
+    return func(getattr(TestCases, f'user_len_list{case}'),
+                getattr(TestCases, f'pos_len_list{case}'),
+                getattr(TestCases, f'pos_rank_sum{case}'))
+
+
+class TestRankMetrics(unittest.TestCase):
+    def test_gauc(self):
+        name = 'gauc'
+        self.assertEqual(get_result(name, case=0), (1 * ((2 - (1 - 1) / 2 - 1 / 1) / (2 - 1)) +
+                                                    2 * ((3 - (2 - 1) / 2 - 4 / 2) / (3 - 2)) +
+                                                    3 * ((5 - (3 - 1) / 2 - 9 / 3) / (5 - 3)))
+                         / (1 + 2 + 3))
+        self.assertEqual(get_result(name, case=1), (3 - 0 - 3 / 1) / (3 - 1))
+
+
+if __name__ == "__main__":
+    unittest.main()
