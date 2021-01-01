3b1b7c8ea537e79a68fe03417139920151c99d43
Lin zihan <893833413@qq.com>
2021-01-01 19:25:18 +0800

    FIX: auto_test

--- tests/model/test_model_auto.py
+++ tests/model/test_model_auto.py
@@ -172,7 +172,7 @@ class TestGeneralRecommender(unittest.TestCase):
     def test_enmf(self):
         config_dict = {
             'model': 'ENMF',
-            'training_neg_sample': 0,
+            'training_neg_sample_num': 0,
         }
         quick_test(config_dict)
 
