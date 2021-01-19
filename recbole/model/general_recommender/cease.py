1ed6cc0c77111f53cb71c00adf956d5d6f729d0c
Deklan Webster <deklanw@gmail.com>
2021-01-19 15:41:04 -0500

    Clean up CEASE

--- recbole/model/general_recommender/cease.py
+++ recbole/model/general_recommender/cease.py
@@ -45,13 +45,32 @@ def encode_categorical_item_features(dataset, included_features):
             encoded_feats.append(encoded)
         else:
             raise Warning(
-                f'CEASE/A-EASE only supports token or token_seq types. [{feat}] is of type [{t}].')
+                f'CEASE only supports token or token_seq types. [{feat}] is of type [{t}].')
 
     if not encoded_feats:
         raise ValueError(
             f'No valid token or token_seq features to include.')
 
     return sp.hstack(encoded_feats).T.astype(np.float32)
+
+
+def ease_like(M, reg_weight):
+    # gram matrix
+    G = M.T @ M
+
+    # add reg to diagonal
+    G += reg_weight * sp.identity(G.shape[0])
+
+    # convert to dense because inverse will be dense
+    G = G.todense()
+
+    # invert. this takes most of the time
+    P = np.linalg.inv(G)
+    B = P / (-np.diag(P))
+    # zero out diag
+    np.fill_diagonal(B, 0.)
+
+    return B
 
 
 class CEASE(GeneralRecommender):
@@ -64,46 +83,28 @@ class CEASE(GeneralRecommender):
         # need at least one param
         self.dummy_param = torch.nn.Parameter(torch.zeros(1))
 
-        B = dataset.inter_matrix(
+        inter_matrix = dataset.inter_matrix(
             form='csr').astype(np.float32)
 
         item_feat_weight = config['item_feat_weight']
         reg_weight = config['reg_weight']
         included_features = config['included_features']
 
-        T = encode_categorical_item_features(dataset, included_features)
-        T *= item_feat_weight
+        tag_item_matrix = item_feat_weight * encode_categorical_item_features(dataset, included_features)
 
         # just directly calculate the entire score matrix in init
         # (can't be done incrementally)
 
-        X = sp.vstack([B, T]).tocsr()
+        X = sp.vstack([inter_matrix, tag_item_matrix]).tocsr()
 
-        # gram matrix
-        G = X.T @ X
-
-        # add reg to diagonal
-        G += reg_weight * sp.identity(G.shape[0])
-
-        # convert to dense because inverse will be dense
-        G = G.todense()
-
-        # invert. this takes most of the time
-        P = np.linalg.inv(G)
-        B = P / (-np.diag(P))
-        # zero out diag
-        np.fill_diagonal(B, 0.)
+        item_similarity = ease_like(X, reg_weight)
 
         # instead of computing and storing the entire score matrix, just store B and compute the scores on demand
         # more memory efficient for a larger number of users
-        # but if there's a large number of items not much one can do:
-        # still have to compute B all at once
-        # S = X @ B
-        # self.score_matrix = torch.from_numpy(S).to(self.device)
 
         # torch doesn't support sparse tensor slicing, so will do everything with np/scipy
-        self.item_similarity = B
-        self.interaction_tag_matrix = X
+        self.item_similarity = item_similarity
+        self.interaction_matrix = inter_matrix
 
     def forward(self):
         pass
@@ -115,10 +116,10 @@ class CEASE(GeneralRecommender):
         user = interaction[self.USER_ID].cpu().numpy()
         item = interaction[self.ITEM_ID].cpu().numpy()
 
-        return torch.from_numpy((self.interaction_tag_matrix[user, :].multiply(self.item_similarity[:, item].T)).sum(axis=1).getA1())
+        return torch.from_numpy((self.interaction_matrix[user, :].multiply(self.item_similarity[:, item].T)).sum(axis=1).getA1())
 
     def full_sort_predict(self, interaction):
         user = interaction[self.USER_ID].cpu().numpy()
 
-        r = self.interaction_tag_matrix[user, :] @ self.item_similarity
+        r = self.interaction_matrix[user, :] @ self.item_similarity
         return torch.from_numpy(r.flatten())
