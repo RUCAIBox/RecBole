94606d77c2665ea4f28157eecc11eb497854c5ba
Yibo-Li-1 <3289438186@qq.com>
2021-02-04 18:22:05 +0800

    ADD RecVAE

--- recbole/model/general_recommender/recvae.py
+++ recbole/model/general_recommender/recvae.py
@@ -8,7 +8,7 @@ RecVAE
 ################################################
 Reference:
     Shenbin, Ilya, et al. "RecVAE: A new variational autoencoder for Top-N recommendations with implicit feedback." Proceedings of the 13th International Conference on Web Search and Data Mining. 2020.
-   
+
 Reference code:
     https://github.com/ilya-shenbin/RecVAE
 """
@@ -24,14 +24,18 @@ from recbole.utils import InputType
 
 
 class Swish(nn.Module):
-    r"""Applies the element-wise activation function: Swish
+    r"""Swish activation function:
+
+    .. math::
+        \text{Swish}(x) = \frac{x}{1 + \exp(-x)}
     """
+
     def forward(self, input):
         return input.mul(torch.sigmoid(input))
 
 
 class RecVAE(GeneralRecommender):
-    r"""Collaborative Denoising Auto-Encoder (RecVAE) is a recommendation model 
+    r"""Collaborative Denoising Auto-Encoder (RecVAE) is a recommendation model
     for top-N recommendation with implicit feedback.
 
     We implement the model following the original author
@@ -48,15 +52,14 @@ class RecVAE(GeneralRecommender):
         self.epsilon = config['epsilon']
         self.mixture_weights = config['mixture_weights']
         self.gamma = config['gamma']
-        
+
         self.history_item_id, self.history_item_value, _ = dataset.history_item_matrix()
         self.history_item_id = self.history_item_id.to(self.device)
         self.history_item_value = self.history_item_value.to(self.device)
 
         self.encode_layer_dims = [self.n_items] + self.layers + [self.latent_dim]
         self.encoder = self.mlp_layers(self.encode_layer_dims)
-        # self.decoder = nn.Linear(self.latent_dim, self.n_items)
-        self.decoder = self.mlp_layers(self.encode_layer_dims[::-1])
+        self.decoder = nn.Linear(self.latent_dim, self.n_items)
 
         # Composite prior
         self.mu_prior = nn.Parameter(torch.Tensor(1, self.latent_dim), requires_grad=False)
@@ -87,7 +90,7 @@ class RecVAE(GeneralRecommender):
         rating_matrix = torch.zeros(1).to(self.device).repeat(user.shape[0], self.n_items)
         rating_matrix.index_put_((row_indices, col_indices), self.history_item_value[user].flatten())
         return rating_matrix
-    
+
     def mlp_layers(self, layer_dims):
         mlp_modules = []
         for i, (d_in, d_out) in enumerate(zip(layer_dims[:-1], layer_dims[1:])):
@@ -100,56 +103,53 @@ class RecVAE(GeneralRecommender):
     def reparameterize(self, mu, logvar):
         if self.training:
             std = torch.exp(0.5 * logvar)
-            eps = torch.randn_like(std)
-            return eps.mul(std).add_(mu)
+            epsilon = torch.zeros_like(std).normal_(mean=0, std=0.01)
+            return mu + epsilon * std
         else:
             return mu
 
     def log_norm_pdf(self, x, mu, logvar):
         return -0.5 * (logvar + np.log(2 * np.pi) + (x - mu).pow(2) / logvar.exp())
 
-    def forward(self, rating_matrix):
+    def forward(self, rating_matrix, drop_out=0):
         h = F.normalize(rating_matrix)
-        h = F.dropout(h, self.dropout_prob, training=self.training)
+        h = F.dropout(h, drop_out, training=self.training)
         h = self.encoder(h)
         mu, logvar = h, h
-        repa = self.reparameterize(mu, logvar)
-        z = self.decoder(repa)
-        return z, mu, logvar, repa
+        r = self.reparameterize(mu, logvar)
+        z = self.decoder(r)
+        return z, mu, logvar, r
 
     def calculate_loss(self, interaction):
         user = interaction[self.USER_ID]
         rating_matrix = self.get_rating_matrix(user)
-        z, mu, logvar, repa = self.forward(rating_matrix)
+        z, mu, logvar, r = self.forward(rating_matrix, self.dropout_prob)
 
         if self.gamma:
-            norm = rating_matrix.sum(dim = -1)
-            norm.div_(norm.max())
-            # print("norm: ", norm)
+            norm = rating_matrix.sum(dim=-1)
             anneal = self.gamma * norm
         else:
             anneal = self.anneal_cap
 
         # Composite prior
         post_mu, post_logvar = self.encoder_old(rating_matrix), self.encoder_old(rating_matrix)
-        stnd_prior = self.log_norm_pdf(repa, self.mu_prior, self.logvar_prior)
-        post_prior = self.log_norm_pdf(repa, post_mu, post_logvar)
-        unif_prior = self.log_norm_pdf(repa, self.mu_prior, self.logvar_uniform_prior)
-        
+        stnd_prior = self.log_norm_pdf(r, self.mu_prior, self.logvar_prior)
+        post_prior = self.log_norm_pdf(r, post_mu, post_logvar)
+        unif_prior = self.log_norm_pdf(r, self.mu_prior, self.logvar_uniform_prior)
+
         gaussians = [stnd_prior, post_prior, unif_prior]
         gaussians = [g.add(np.log(w)) for g, w in zip(gaussians, self.mixture_weights)]
-        density_per_gaussian = torch.stack(gaussians, dim = -1)
-                
-        com_prior = torch.logsumexp(density_per_gaussian, dim = -1)
+        density_per_gaussian = torch.stack(gaussians, dim=-1)
+
+        com_prior = torch.logsumexp(density_per_gaussian, dim=-1)
 
         # KL loss
-        kl_loss = (self.log_norm_pdf(repa, mu, logvar) - com_prior).sum(dim = -1).mul(anneal).mean()
+        kl_loss = (self.log_norm_pdf(r, mu, logvar) - com_prior).sum(dim=-1).mul(anneal).mean()
 
         # CE loss
-        ce_loss = -(F.log_softmax(z, 1) * rating_matrix).sum(dim = -1).mean()
+        ce_loss = -(F.log_softmax(z, 1) * rating_matrix).sum(dim=-1).mean()
 
         return kl_loss + ce_loss
-        # return ce_loss
 
     def predict(self, interaction):
         user = interaction[self.USER_ID]
@@ -157,7 +157,7 @@ class RecVAE(GeneralRecommender):
 
         rating_matrix = self.get_rating_matrix(user)
 
-        scores, _, _, _ = self.forward(rating_matrix)
+        scores, _, _, _ = self.forward(rating_matrix, self.dropout_prob)
 
         return scores[[user, item]]
 
@@ -166,6 +166,6 @@ class RecVAE(GeneralRecommender):
 
         rating_matrix = self.get_rating_matrix(user)
 
-        scores, _, _, _ = self.forward(rating_matrix)
+        scores, _, _, _ = self.forward(rating_matrix, self.dropout_prob)
 
         return scores.view(-1)
