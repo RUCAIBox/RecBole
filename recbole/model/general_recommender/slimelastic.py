a5491896f26b4bb0fbea2b382c3f8816194308e7
ShanleiMu <2015201909@ruc.edu.cn>
2021-03-03 21:18:56 +0800

    FIX: reformat the general model SLIMElastic

--- recbole/model/general_recommender/slimelastic.py
+++ recbole/model/general_recommender/slimelastic.py
@@ -2,62 +2,58 @@ r"""
 SLIMElastic
 ################################################
 Reference:
-    10.1109/ICDM.2011.134
-    https://www.slideshare.net/MarkLevy/efficient-slides
-
+    Xia Ning et al. "SLIM: Sparse Linear Methods for Top-N Recommender Systems." in ICDM 2011.
 Reference code:
     https://github.com/KarypisLab/SLIM
     https://github.com/MaurizioFD/RecSys2019_DeepLearning_Evaluation/blob/master/SLIM_ElasticNet/SLIMElasticNetRecommender.py
 """
 
 
-from recbole.utils.enum_type import ModelType
-import numpy as np
-import scipy.sparse as sp
 import torch
 import warnings
+import numpy as np
+import scipy.sparse as sp
+
 from sklearn.linear_model import ElasticNet
 from sklearn.exceptions import ConvergenceWarning
 
-from recbole.utils import InputType
+from recbole.utils import InputType, ModelType
 from recbole.model.abstract_recommender import GeneralRecommender
 
 
-# https://github.com/RUCAIBox/RecBole/issues/622
-def add_noise(t, mag=1e-5):
-    return t + mag * torch.rand(t.shape)
-
-
 class SLIMElastic(GeneralRecommender):
+    r"""SLIMElastic is a sparse linear method for top-K recommendation, which learns
+    a sparse aggregation coefficient matrix by solving an L1-norm and L2-norm
+    regularized optimization problem. 
+
+    """
     input_type = InputType.POINTWISE
     type = ModelType.TRADITIONAL
 
     def __init__(self, config, dataset):
         super().__init__(config, dataset)
 
+        # load parameters info
+        self.hide_item = config['hide_item']
+        self.alpha = config['alpha']
+        self.l1_ratio = config['l1_ratio']
+        self.positive_only = config['positive_only']
+
         # need at least one param
         self.dummy_param = torch.nn.Parameter(torch.zeros(1))
 
-        X = dataset.inter_matrix(
-            form='csr').astype(np.float32)
-
+        X = dataset.inter_matrix(form='csr').astype(np.float32)
         X = X.tolil()
         self.interaction_matrix = X
 
-        hide_item = config['hide_item']
-        alpha = config['alpha']
-        l1_ratio = config['l1_ratio']
-        positive_only = config['positive_only']
-
-        model = ElasticNet(alpha=alpha, l1_ratio=l1_ratio,
-                           positive=positive_only,
+        model = ElasticNet(alpha=self.alpha, l1_ratio=self.l1_ratio,
+                           positive=self.positive_only,
                            fit_intercept=False,
                            copy_X=False,
                            precompute=True,
                            selection='random',
                            max_iter=100,
                            tol=1e-4)
-
         item_coeffs = []
 
         # ignore ConvergenceWarnings
@@ -68,7 +64,7 @@ class SLIMElastic(GeneralRecommender):
                 # target column
                 r = X[:, j]
 
-                if hide_item:
+                if self.hide_item:
                     # set item column to 0
                     X[:, j] = 0
 
@@ -80,7 +76,7 @@ class SLIMElastic(GeneralRecommender):
 
                 item_coeffs.append(coeffs)
 
-                if hide_item:
+                if self.hide_item:
                     # reattach column if removed
                     X[:, j] = r
 
@@ -99,7 +95,7 @@ class SLIMElastic(GeneralRecommender):
         r = torch.from_numpy((self.interaction_matrix[user, :].multiply(
             self.item_similarity[:, item].T)).sum(axis=1).getA1())
 
-        return add_noise(r)
+        return r
 
     def full_sort_predict(self, interaction):
         user = interaction[self.USER_ID].cpu().numpy()
@@ -107,4 +103,4 @@ class SLIMElastic(GeneralRecommender):
         r = self.interaction_matrix[user, :] @ self.item_similarity
         r = torch.from_numpy(r.todense().getA1())
 
-        return add_noise(r)
+        return r
