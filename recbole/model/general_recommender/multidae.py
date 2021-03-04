3e8cc1298f29ea68c5bd61d26ecdf7d05d38ec26
Yupeng Hou <houyupeng@ruc.edu.cn>
2021-03-04 00:50:23 +0000

    FIX: typos in VAE models

--- recbole/model/general_recommender/multidae.py
+++ recbole/model/general_recommender/multidae.py
@@ -22,7 +22,7 @@ from recbole.utils import InputType
 
 
 class MultiDAE(GeneralRecommender):
-    r"""MultiDAE is a item-based model collaborative filtering model that simultaneously rank all items for user .
+    r"""MultiDAE is an item-based collaborative filtering model that simultaneously ranks all items for each user.
 
     We implement the the MultiDAE model with only user dataloader.
     """
