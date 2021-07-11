0a36bb1f82862d8a5aa48ba3c5bcc536d8faff6e
Yupeng Hou <houyupeng@ruc.edu.cn>
2021-07-11 05:05:11 +0000

    FEA: add test for seq split by ratio

--- tests/data/test_dataset.py
+++ tests/data/test_dataset.py
@@ -4,7 +4,7 @@
 # @Email  : chenyushuo@ruc.edu.cn
 
 # UPDATE
-# @Time    :   2020/1/3, 2021/7/1, 2021/7/9
+# @Time    :   2020/1/3, 2021/7/1, 2021/7/11
 # @Author  :   Yushuo Chen, Xingyu Pan, Yupeng Hou
 # @email   :   chenyushuo@ruc.edu.cn, xy_pan@foxmail.com, houyupeng@ruc.edu.cn
 
@@ -611,6 +611,48 @@ class TestSeqDataset:
             [0., 0., 0., 0., 0., 0., 1., 0., 0.],
             [0., 0., 0., 0., 0., 0., 0., 0., 0.]
         ]).all()
+
+    def test_seq_split_by_ratio(self):
+        config_dict = {
+            'model': 'GRU4Rec',
+            'dataset': 'seq_dataset',
+            'data_path': current_path,
+            'load_col': None,
+            'training_neg_sample_num': 0,
+            'eval_args': {
+                'split': {'RS': [0.3, 0.3, 0.4]},
+                'order': 'TO'
+            }
+        }
+        train_dataset, valid_dataset, test_dataset = split_dataset(config_dict=config_dict)
+        assert (train_dataset.inter_feat[train_dataset.uid_field].numpy() == [1, 1, 1, 4, 2, 2, 3]).all()
+        assert (train_dataset.inter_feat[train_dataset.item_id_list_field][:,:3].numpy() == [
+            [1, 0, 0],
+            [1, 2, 0],
+            [1, 2, 3],
+            [3, 0, 0],
+            [4, 0, 0],
+            [4, 5, 0],
+            [4, 0, 0]]).all()
+        assert (train_dataset.inter_feat[train_dataset.iid_field].numpy() == [2, 3, 4, 4, 5, 6, 5]).all()
+        assert (train_dataset.inter_feat[train_dataset.item_list_length_field].numpy() == [1, 2, 3, 1, 1, 2, 1]).all()
+
+        assert (valid_dataset.inter_feat[valid_dataset.uid_field].numpy() == [1, 1, 2]).all()
+        assert (valid_dataset.inter_feat[valid_dataset.item_id_list_field][:,:5].numpy() == [
+            [1, 2, 3, 4, 0],
+            [1, 2, 3, 4, 5],
+            [4, 5, 6, 0, 0]]).all()
+        assert (valid_dataset.inter_feat[valid_dataset.iid_field].numpy() == [5, 6, 7]).all()
+        assert (valid_dataset.inter_feat[valid_dataset.item_list_length_field].numpy() == [4, 5, 3]).all()
+
+        assert (test_dataset.inter_feat[test_dataset.uid_field].numpy() == [1, 1, 2, 3]).all()
+        assert (test_dataset.inter_feat[test_dataset.item_id_list_field][:,:7].numpy() == [
+            [1, 2, 3, 4, 5, 6, 0],
+            [1, 2, 3, 4, 5, 6, 7],
+            [4, 5, 6, 7, 0, 0, 0],
+            [4, 5, 0, 0, 0, 0, 0]]).all()
+        assert (test_dataset.inter_feat[test_dataset.iid_field].numpy() == [7, 8, 8, 6]).all()
+        assert (test_dataset.inter_feat[test_dataset.item_list_length_field].numpy() == [6, 7, 4, 2]).all()
 
 
 class TestKGDataset:
