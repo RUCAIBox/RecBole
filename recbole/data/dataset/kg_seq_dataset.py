31c906cd314e713931ea8b778ffb60f16c1ed3ef
chenyushuo <297086016@qq.com>
2021-08-20 10:48:46 +0800

    REFACTOR: rename `Kg_Seq_Dataset` to `KGSeqDataset`.

--- recbole/data/dataset/kg_seq_dataset.py
+++ recbole/data/dataset/kg_seq_dataset.py
@@ -10,7 +10,7 @@ recbole.data.kg_seq_dataset
 from recbole.data.dataset import SequentialDataset, KnowledgeBasedDataset
 
 
-class Kg_Seq_Dataset(SequentialDataset, KnowledgeBasedDataset):
+class KGSeqDataset(SequentialDataset, KnowledgeBasedDataset):
     """Containing both processing of Sequential Models and Knowledge-based Models.
 
     Inherit from :class:`~recbole.data.dataset.sequential_dataset.SequentialDataset` and
