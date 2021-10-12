5448623a9f9d39e6aa1a2b5f8e2e9eb52d9d1797
Sherry-XLL <xulanling_sherry@163.com>
2021-10-12 03:23:27 +0000

    FEA: Add LightSANs in Sequential models

--- run_hyper.py
+++ run_hyper.py
@@ -18,6 +18,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--config_files', type=str, default=None, help='fixed config files')
     parser.add_argument('--params_file', type=str, default=None, help='parameters file')
+    parser.add_argument('--output_file', type=str, default='hyper_example.result', help='output file')
     args, _ = parser.parse_known_args()
 
     # plz set algo='exhaustive' to use exhaustive search, in this case, max_evals is auto set
@@ -25,7 +26,7 @@ def main():
     hp = HyperTuning(objective_function, algo='exhaustive',
                      params_file=args.params_file, fixed_config_file_list=config_file_list)
     hp.run()
-    hp.export_result(output_file='hyper_example.result')
+    hp.export_result(output_file=args.output_file)
     print('best params: ', hp.best_params)
     print('best result: ')
     print(hp.params2result[hp.params2str(hp.best_params)])
