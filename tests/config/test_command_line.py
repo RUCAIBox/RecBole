787b7fac009ec570a5059db36e7ff3f7b70859e6
chenyushuo <297086016@qq.com>
2022-02-21 19:45:53 +0800

    FIX: bug fix for test_command_line.py and run_test.sh

--- tests/config/test_command_line.py
+++ tests/config/test_command_line.py
@@ -13,21 +13,12 @@ from recbole.config import Config
 
 if __name__ == '__main__':
 
-    parameters_dict = {
-        'model': 'SASRec',
-        'learning_rate': 0.2,
-        'topk': [50, 100],
-        'epochs': 100,
-    }
-
-    config_file_list = ['test_config_example.yaml']
-
     config = Config(model='BPR', dataset='ml-100k')
 
     # command line
-    assert config['use_gpu'] == False
+    assert config['use_gpu'] is False
     assert config['valid_metric'] == 'Recall@10'
-    # assert config['metrics'] == ['Recall@10']     # bug
+    assert config['metrics'] == ['Recall']     # bug
 
     # priority
     assert config['epochs'] == 200
