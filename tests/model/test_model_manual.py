d7759641ebd40cf1552792b7fcdae55713d54689
Wicknight <1462034631@qq.com>
2022-07-12 07:56:17 +0800

    Refactor: CI test for train_neg_sample_args

--- tests/model/test_model_manual.py
+++ tests/model/test_model_manual.py
@@ -30,7 +30,7 @@ class TestSequentialRecommender(unittest.TestCase):
             'model': 'S3Rec',
             'train_stage': 'pretrain',
             'save_step': 1,
-            'neg_sampling': None
+            'train_neg_sample_args': None
         }
         quick_test(config_dict)
 
@@ -38,7 +38,7 @@ class TestSequentialRecommender(unittest.TestCase):
             'model': 'S3Rec',
             'train_stage': 'finetune',
             'pre_model_path': './saved/S3Rec-test-1.pth',
-            'neg_sampling': None
+            'train_neg_sample_args': None
         }
         quick_test(config_dict)
 
