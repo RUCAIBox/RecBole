869778aa48edd213ef050573c147ba766ca38755
Wicknight <Wicknight@users.noreply.github.com>
2023-02-15 07:12:51 +0000

    Format Python code according to PEP8

--- recbole/utils/utils.py
+++ recbole/utils/utils.py
@@ -401,7 +401,9 @@ def list_to_latex(convert_list, bigger_flag=True, subset_columns=[]):
     style = df.style
     style.apply(bold_func, bigger_flag=bigger_flag, subset=subset_columns)
     style.format(precision=4)
-    tex = style.hide(axis='index').to_latex(label="Result", convert_css=True, hrules=True)
+    tex = style.hide(axis="index").to_latex(
+        label="Result", convert_css=True, hrules=True
+    )
     return df, tex
 
 
