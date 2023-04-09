da6b1dc5826d87ae853559cc88943e0b382cbf69
ElaineL610 <jiafeimao14614@126.com>
2023-04-09 18:50:52 +0800

    create file

---
+++ recbole/model/context_aware_recommender/fm2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time   : 2023/4/9
+# @Author : Yilin Liu
+# @Email  : liu_elaine2022@outlook.com
+# @File   : fm2.py
+
+r"""
+FmFM
+#####################################################
+Reference:
+    Sun Y, Pan J, Zhang A, et al. “FM2: Field-matrixed Factorization Machines for Recommender Systems.” in WWW 2021.
+"""
+import torch
