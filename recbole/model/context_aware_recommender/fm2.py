3fb16fc5f085f8b5b15cc16d15eb293ea062eae4
ElaineL610 <jiafeimao14614@126.com>
2023-04-17 13:26:38 +0800

    run successful

--- recbole/model/context_aware_recommender/fm2.py
+++ recbole/model/context_aware_recommender/fm2.py
@@ -17,22 +17,16 @@ from torch.nn.init import xavier_normal_, constant_
 from recbole.model.abstract_recommender import ContextRecommender
 
 class FM2(ContextRecommender):
-    """"""
+    """
+        Field-matrixed Factorization Machines (FmFM, or 𝐹M^2 ), propose a novel approach FmFM to model the
+interactions of field pairs as a matrix.
+    """
     def __init__(self, config, dataset):
         super(FM2, self).__init__(config, dataset)
 
-        # self.embedding_layer = EmbeddingLayer(feature_map, embedding_dim)
-        self.embedding_dim = config["embedding_dim"]
-
+        self.embedding_dim = config["embedding_size"]
         self.num_fields = self.num_feature_field
-
-        self.interact_dim = int(self.num_fields * (self.num_fields - 1) / 2)  #
-
-
-        # self.fields = config["fields"]
-        # self.num_features = self.num_feature_field
-
-        # self.embedding_dim = config["embedding_dim"]
+        self.interact_dim = int(self.num_fields * (self.num_fields - 1) / 2)
 
         self.sigmoid = nn.Sigmoid()
         self.loss = nn.BCEWithLogitsLoss()
@@ -40,25 +34,12 @@ class FM2(ContextRecommender):
         self.weight = torch.randn(
             self.interact_dim, self.embedding_dim, self.embedding_dim, requires_grad=True, device=self.device
         )
+
         # parameters initialization
         self.apply(self._init_weights)
 
+        # used to mark cross items
         self.triu_index = torch.triu(torch.ones(self.num_fields, self.num_fields), 1).nonzero().to(self.device)
-
-        # self.feature_names = (
-        #     self.token_field_names,
-        #     self.token_seq_field_names,
-        #     self.float_field_names,
-        # )
-        # self.feat_dims = (
-        #     self.token_field_dims,
-        #     self.token_seq_field_dims,
-        #     self.float_field_dims,
-        # )
-        # self.interaction_weight = nn.Parameter(torch.Tensor(self.interact_dim, self.embedding_dim, self.embedding_dim))
-
-
-
 
     def _init_weights(self, module):
         if isinstance(module, nn.Embedding):
@@ -69,63 +50,13 @@ class FM2(ContextRecommender):
                 constant_(module.bias.data, 0)
 
     def fm2_matrix(self, infeature):
-
-        # batch_size = infeature.shape[0]
-        # fm2_inter = []
-        # for i, feature_emb in enumerate(infeature):
-        #
-        #     left_emb = torch.index_select(feature_emb, 1, self.triu_index[:, 0])
-        #     right_emb = torch.index_select(feature_emb, 1, self.triu_index[:, 1])
-        #     left_emb = torch.matmul(left_emb.unsqueeze(2), self.weight).squeeze(2)
-        #     fm2_in = (left_emb * right_emb).sum(dim=-1).sum(dim=-1, keepdim=True)
-        #     fm2_inter.append(fm2_in)
-
-        feature_emb = infeature
-        left_emb = torch.index_select(feature_emb, 1, self.triu_index[:, 0])
-        right_emb = torch.index_select(feature_emb, 1, self.triu_index[:, 1])
-        left_emb = torch.matmul(left_emb, self.weight).squeeze(2)
-        fm2_output = (left_emb * right_emb).sum(dim=-1).sum(dim=-1, keepdim=True)
-
-        # # Reduce to [N, 1]
-        # fm2_output = torch.sum(fm2_inter, dim=1, keepdim=True)  # [batch_size, 1]
-
-        # weight = self.weight.expand(batch_size, -1, -1, -1)
-        # print(infeature)
-
-
-
-
-        # l_left, l_right = [], []
-        # field_order = sorted(self.feature_dims.items(), key=lambda x: (-x[1], x[0]))
-        # for idx_l, (feat_l, dim_l) in enumerate(field_order):
-        #     for idx_r, (feat_r, dim_r) in enumerate(field_order[idx_l + 1:]):
-        #         idx_r += (idx_l + 1)
-        #         name = '%s_%s' % (feat_l, feat_r)
-        #         name_alt = '%s_%s' % (feat_r, feat_l)
-        #         if self.cross_fields and name not in self.cross_fields and name_alt not in self.cross_fields:
-        #             continue
-        #
-        #         l_left.append(torch.matmul(self.xv[feat_l], w))
-        #         l_right.append(self.xv[feat_r])
-        #
-        #     l_left = torch.cat(l_left, 1)
-        #     l_right = torch.cat(l_right, 1)
-        #     fm2_inter = torch.mul(l_left, l_right)
-        #
-        #
-        #     # Reduce to [N, 1]
-        #     fm2_output = torch.sum(fm2_inter, dim=1, keepdim=True)  # [batch_size, 1]
-        #
-        #     batch_size = infeature.shape[0]
-        #     weight = self.weight.expand(batch_size, -1, -1, -1)
-        #
-        #     fwfm_inter = list()  # [batch_size, num_fields, emb_dim]
-        #     for i in range(self.num_features - 1):
-        #         for j in range(i + 1, self.num_features):
-        #             Fi, Fj = self.feature2field[i], self.feature2field[j]
-        #             fwfm_inter.append(infeature[:, i] * infeature[:, j] * weight[:, Fi, Fj])
-        #     fwfm_inter = torch.stack(fwfm_inter, dim=1)
-        #     fwfm_inter = torch.sum(fwfm_inter, dim=1)  # [batch_size, emb_dim]
+        """
+            FmFM interaction terms calculation
+        """
+        left_emb = torch.index_select(infeature, 1, self.triu_index[:, 0]) # [batch_size, interact_dim, embed_dim]
+        right_emb = torch.index_select(infeature, 1, self.triu_index[:, 1])
+        left_emb = torch.matmul(left_emb.unsqueeze(2), self.weight).squeeze(2)
+        fm2_output = (left_emb * right_emb).sum(dim=-1).sum(dim=-1, keepdim=True) # [batch_size, 1]
 
         return fm2_output
 
@@ -139,7 +70,6 @@ class FM2(ContextRecommender):
         )
 
         return output.squeeze(-1)
-
 
     def calculate_loss(self, interaction):
         label = interaction[self.LABEL]
