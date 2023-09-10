e09f196b88bfe85db6e88f271d4c427d54bc8379
BishopLiu <BishopLiu@users.noreply.github.com>
2023-09-10 15:38:19 +0000

    Format Python code according to PEP8

--- recbole/model/sequential_recommender/bert4rec.py
+++ recbole/model/sequential_recommender/bert4rec.py
@@ -31,7 +31,6 @@ from recbole.model.layers import TransformerEncoder
 
 
 class BERT4Rec(SequentialRecommender):
-
     def __init__(self, config, dataset):
         super(BERT4Rec, self).__init__(config, dataset)
 
@@ -39,7 +38,9 @@ class BERT4Rec(SequentialRecommender):
         self.n_layers = config["n_layers"]
         self.n_heads = config["n_heads"]
         self.hidden_size = config["hidden_size"]  # same as embedding_size
-        self.inner_size = config["inner_size"]  # the dimensionality in feed-forward layer
+        self.inner_size = config[
+            "inner_size"
+        ]  # the dimensionality in feed-forward layer
         self.hidden_dropout_prob = config["hidden_dropout_prob"]
         self.attn_dropout_prob = config["attn_dropout_prob"]
         self.hidden_act = config["hidden_act"]
@@ -60,8 +61,12 @@ class BERT4Rec(SequentialRecommender):
         self.mask_item_length = int(self.mask_ratio * self.max_seq_length)
 
         # define layers and loss
-        self.item_embedding = nn.Embedding(self.n_items + 1, self.hidden_size, padding_idx=0)  # mask token add 1
-        self.position_embedding = nn.Embedding(self.max_seq_length, self.hidden_size)  # add mask_token at the last
+        self.item_embedding = nn.Embedding(
+            self.n_items + 1, self.hidden_size, padding_idx=0
+        )  # mask token add 1
+        self.position_embedding = nn.Embedding(
+            self.max_seq_length, self.hidden_size
+        )  # add mask_token at the last
         self.trm_encoder = TransformerEncoder(
             n_layers=self.n_layers,
             n_heads=self.n_heads,
@@ -104,7 +109,9 @@ class BERT4Rec(SequentialRecommender):
         """
         Add mask token at the last position according to the lengths of item_seq
         """
-        padding = torch.zeros(item_seq.size(0), dtype=torch.long, device=item_seq.device)  # [B]
+        padding = torch.zeros(
+            item_seq.size(0), dtype=torch.long, device=item_seq.device
+        )  # [B]
         item_seq = torch.cat((item_seq, padding.unsqueeze(-1)), dim=-1)  # [B max_len+1]
         for batch_id, last_position in enumerate(item_seq_len):
             item_seq[batch_id][last_position] = self.mask_token
@@ -112,7 +119,9 @@ class BERT4Rec(SequentialRecommender):
         return item_seq
 
     def forward(self, item_seq):
-        position_ids = torch.arange(item_seq.size(1), dtype=torch.long, device=item_seq.device)
+        position_ids = torch.arange(
+            item_seq.size(1), dtype=torch.long, device=item_seq.device
+        )
         position_ids = position_ids.unsqueeze(0).expand_as(item_seq)
         position_embedding = self.position_embedding(position_ids)
         item_emb = self.item_embedding(item_seq)
@@ -120,7 +129,9 @@ class BERT4Rec(SequentialRecommender):
         input_emb = self.LayerNorm(input_emb)
         input_emb = self.dropout(input_emb)
         extended_attention_mask = self.get_attention_mask(item_seq, bidirectional=True)
-        trm_output = self.trm_encoder(input_emb, extended_attention_mask, output_all_encoded_layers=True)
+        trm_output = self.trm_encoder(
+            input_emb, extended_attention_mask, output_all_encoded_layers=True
+        )
         ffn_output = self.output_ffn(trm_output[-1])
         output = self.output_gelu(ffn_output)
         return output  # [B L H]
@@ -143,7 +154,9 @@ class BERT4Rec(SequentialRecommender):
             multi_hot_embed: [[0 1 0 0 0], [0 0 0 1 0]]
         """
         masked_index = masked_index.view(-1)
-        multi_hot = torch.zeros(masked_index.size(0), max_length, device=masked_index.device)
+        multi_hot = torch.zeros(
+            masked_index.size(0), max_length, device=masked_index.device
+        )
         multi_hot[torch.arange(masked_index.size(0)), masked_index] = 1
         return multi_hot
 
@@ -154,9 +167,13 @@ class BERT4Rec(SequentialRecommender):
         masked_index = interaction[self.MASK_INDEX]
 
         seq_output = self.forward(masked_item_seq)
-        pred_index_map = self.multi_hot_embed(masked_index, masked_item_seq.size(-1))  # [B*mask_len max_len]
+        pred_index_map = self.multi_hot_embed(
+            masked_index, masked_item_seq.size(-1)
+        )  # [B*mask_len max_len]
         # [B mask_len] -> [B mask_len max_len] multi hot
-        pred_index_map = pred_index_map.view(masked_index.size(0), masked_index.size(1), -1)  # [B mask_len max_len]
+        pred_index_map = pred_index_map.view(
+            masked_index.size(0), masked_index.size(1), -1
+        )  # [B mask_len max_len]
         # [B mask_len max_len] * [B max_len H] -> [B mask_len H]
         # only calculate loss for masked position
         seq_output = torch.bmm(pred_index_map, seq_output)  # [B mask_len H]
@@ -164,20 +181,33 @@ class BERT4Rec(SequentialRecommender):
         if self.loss_type == "BPR":
             pos_items_emb = self.item_embedding(pos_items)  # [B mask_len H]
             neg_items_emb = self.item_embedding(neg_items)  # [B mask_len H]
-            pos_score = torch.sum(seq_output * pos_items_emb, dim=-1) + self.output_bias[pos_items]  # [B mask_len]
-            neg_score = torch.sum(seq_output * neg_items_emb, dim=-1) + self.output_bias[neg_items]  # [B mask_len]
+            pos_score = (
+                torch.sum(seq_output * pos_items_emb, dim=-1)
+                + self.output_bias[pos_items]
+            )  # [B mask_len]
+            neg_score = (
+                torch.sum(seq_output * neg_items_emb, dim=-1)
+                + self.output_bias[neg_items]
+            )  # [B mask_len]
             targets = (masked_index > 0).float()
-            loss = -torch.sum(torch.log(1e-14 + torch.sigmoid(pos_score - neg_score)) * targets) / torch.sum(targets)
+            loss = -torch.sum(
+                torch.log(1e-14 + torch.sigmoid(pos_score - neg_score)) * targets
+            ) / torch.sum(targets)
             return loss
 
         elif self.loss_type == "CE":
             loss_fct = nn.CrossEntropyLoss(reduction="none")
-            test_item_emb = self.item_embedding.weight[:self.n_items]  # [item_num H]
-            logits = torch.matmul(seq_output, test_item_emb.transpose(0, 1)) + self.output_bias  # [B mask_len item_num]
+            test_item_emb = self.item_embedding.weight[: self.n_items]  # [item_num H]
+            logits = (
+                torch.matmul(seq_output, test_item_emb.transpose(0, 1))
+                + self.output_bias
+            )  # [B mask_len item_num]
             targets = (masked_index > 0).float().view(-1)  # [B*mask_len]
 
-            loss = torch.sum(loss_fct(logits.view(-1, test_item_emb.size(0)), pos_items.view(-1)) * targets
-                             ) / torch.sum(targets)
+            loss = torch.sum(
+                loss_fct(logits.view(-1, test_item_emb.size(0)), pos_items.view(-1))
+                * targets
+            ) / torch.sum(targets)
             return loss
         else:
             raise NotImplementedError("Make sure 'loss_type' in ['BPR', 'CE']!")
@@ -190,7 +220,9 @@ class BERT4Rec(SequentialRecommender):
         seq_output = self.forward(item_seq)
         seq_output = self.gather_indexes(seq_output, item_seq_len - 1)  # [B H]
         test_item_emb = self.item_embedding(test_item)
-        scores = (torch.mul(seq_output, test_item_emb)).sum(dim=1) + self.output_bias[test_item]  # [B]
+        scores = (torch.mul(seq_output, test_item_emb)).sum(dim=1) + self.output_bias[
+            test_item
+        ]  # [B]
         return scores
 
     def full_sort_predict(self, interaction):
@@ -199,6 +231,10 @@ class BERT4Rec(SequentialRecommender):
         item_seq = self.reconstruct_test_data(item_seq, item_seq_len)
         seq_output = self.forward(item_seq)
         seq_output = self.gather_indexes(seq_output, item_seq_len - 1)  # [B H]
-        test_items_emb = self.item_embedding.weight[:self.n_items]  # delete masked token
-        scores = torch.matmul(seq_output, test_items_emb.transpose(0, 1)) + self.output_bias  # [B, item_num]
+        test_items_emb = self.item_embedding.weight[
+            : self.n_items
+        ]  # delete masked token
+        scores = (
+            torch.matmul(seq_output, test_items_emb.transpose(0, 1)) + self.output_bias
+        )  # [B, item_num]
         return scores
