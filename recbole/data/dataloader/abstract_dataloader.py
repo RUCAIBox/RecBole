6d9f74216379ccfac068be9769cb3a2858d4fd14
zhengbw0324 <zhengbw0324@users.noreply.github.com>
2023-09-26 01:10:42 +0000

    Format Python code according to PEP8

--- recbole/data/dataloader/abstract_dataloader.py
+++ recbole/data/dataloader/abstract_dataloader.py
@@ -136,10 +136,9 @@ class NegSampleDataLoader(AbstractDataLoader):
         self.neg_sample_args = neg_sample_args
         self.times = 1
         if (
-            (self.neg_sample_args["distribution"] == "uniform"
-            or self.neg_sample_args["distribution"] == "popularity")
-            and self.neg_sample_args["sample_num"] != "none"
-        ):
+            self.neg_sample_args["distribution"] == "uniform"
+            or self.neg_sample_args["distribution"] == "popularity"
+        ) and self.neg_sample_args["sample_num"] != "none":
             self.neg_sample_num = self.neg_sample_args["sample_num"]
 
             if self.dl_format == InputType.POINTWISE:
